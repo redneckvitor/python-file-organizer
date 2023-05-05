f8641589e5af7f56a15b7bff6c4ace1e64bcda30
João Vítor Rodrigues <98863343+redneckvitor@users.noreply.github.com>
2023-05-05 00:53:49 -0300

    Suporte para mais formatos de imagem, e videos
    Agora o programa reconhece mais formatos de de imagem, e organiza arquivos de vídeos

--- main.py
+++ main.py
@@ -2,25 +2,42 @@ import os
 import shutil
 
 downloads_folder = os.path.expanduser("~/Downloads")
-images_folder = os.path.expanduser("~/Downloads/images")
 
-os.makedirs(images_folder, exist_ok=True)
+image_extensions = [".png", ".jpg", ".jpeg", ".tif", ".tga", ".webp"]
+video_extensions = [".mkv", ".mov", ".mp4", ".webm"]
 
 
 def organize_files(path):
-    """Recebe uma diretório no parâmetro 'path' e organiza os arquivos"""
+    """Recebe um diretório no parâmetro 'path' e organiza os arquivos"""
+
+    os.makedirs(f'{path}/images', exist_ok=True)  # Cria uma nova pasta 'images' dentro do diretório escolhido
+    os.makedirs(f'{path}/videos', exist_ok=True)  # Cria uma nova pasta 'videos' dentro do diretório escolhido
+
 
     for file_name in os.listdir(path):
+        # Realiza uma operação para cada arquivo da pasta
 
-        if file_name.endswith(".png") or file_name.endswith(".jpg"):
-            file_path = os.path.join(downloads_folder, file_name)
+        # Abaixo função para organizar os arquivos de imagem
+        if any(file_name.endswith(extension) for extension in image_extensions):
+            file_path = os.path.join(path, file_name)
 
             try:
-                shutil.move(file_path, images_folder)
+                shutil.move(file_path, f'{path}/images')
             except shutil.Error:
-                print(f"{file_name} already exists in the destination folder")
+                print(f"{file_name} um arquivo de mesmo nome já existe na pasta")
+
+        # Abaixo função para organizar os arquivos de video
+        if any(file_name.endswith(extension) for extension in video_extensions):
+            file_path = os.path.join(path, file_name)
+
+            try:
+                shutil.move(file_path, f'{path}/videos')
+            except shutil.Error:
+                print(f"{file_name} um arquivo de mesmo nome já existe na pasta")
+
+
 
         print(file_name)  # debug
 
 
-organize_files(downloads_folder)
+organize_files(downloads_folder) # Coloque aqui o caminho do diretório que você quer organizar
