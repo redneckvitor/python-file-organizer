91e95afed4f964930370bc58f4580f78df9306be
João Vítor Rodrigues <98863343+redneckvitor@users.noreply.github.com>
2023-05-05 02:04:47 -0300

    Código otimizado e e refatorado
    Adicionado suporte a mais tipos de arquivos, código refatorado para uma função geral

--- main.py
+++ main.py
@@ -5,39 +5,30 @@ downloads_folder = os.path.expanduser("~/Downloads")
 
 image_extensions = [".png", ".jpg", ".jpeg", ".tif", ".tga", ".webp"]
 video_extensions = [".mkv", ".mov", ".mp4", ".webm"]
+compressed_extensions = [".zip", ".7z", ".rar"]
+document_extensions = [".pdf", ".epub", ".txt", ".html", ".docx", ".csv", ".psd"]
 
 
-def organize_files(path):
-    """Recebe um diretório no parâmetro 'path' e organiza os arquivos"""
+def organize_files(path, type='images', extension_list=image_extensions):
+    """Separa os arquivos de um diretório, criando novas pastas.
+    'path' = o caminho do diretório a ser organizado
+    'type' = nome da pasta que será criada e onde os arquivos serão movidos
+    'extension_list' = lista das extensões de arquivos a serem separados
+    """
 
-    os.makedirs(f'{path}/images', exist_ok=True)  # Cria uma nova pasta 'images' dentro do diretório escolhido
-    os.makedirs(f'{path}/videos', exist_ok=True)  # Cria uma nova pasta 'videos' dentro do diretório escolhido
-
+    os.makedirs(f'{path}/{type}', exist_ok=True)
 
     for file_name in os.listdir(path):
-        # Realiza uma operação para cada arquivo da pasta
 
-        # Abaixo função para organizar os arquivos de imagem
-        if any(file_name.endswith(extension) for extension in image_extensions):
+        if any(file_name.endswith(extension) for extension in extension_list):
             file_path = os.path.join(path, file_name)
 
             try:
-                shutil.move(file_path, f'{path}/images')
+                shutil.move(file_path, f'{path}/{type}')
             except shutil.Error:
                 print(f"{file_name} um arquivo de mesmo nome já existe na pasta")
-
-        # Abaixo função para organizar os arquivos de video
-        if any(file_name.endswith(extension) for extension in video_extensions):
-            file_path = os.path.join(path, file_name)
-
-            try:
-                shutil.move(file_path, f'{path}/videos')
-            except shutil.Error:
-                print(f"{file_name} um arquivo de mesmo nome já existe na pasta")
-
-
 
         print(file_name)  # debug
 
 
-organize_files(downloads_folder) # Coloque aqui o caminho do diretório que você quer organizar
+organize_files('test_folder', 'images', image_extensions)
