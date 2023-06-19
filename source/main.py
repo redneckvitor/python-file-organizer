11ab6705652714c846566245d503be98d4b09145
steingames <joaovitorpessoal@protonmail.com>
2023-06-19 14:54:35 -0300

    Changed output log feature

--- source/main.py
+++ source/main.py
@@ -1,30 +1,20 @@
 import os
+import hashlib
 import shutil
 from pathlib import Path
-
 import tkinter as tk
 from tkinter import filedialog
 
-# Cria o arquivo de log de saida se ele não existir e caso exista
-# ele limpa o arquivo
-Path('output_log.txt').write_text('')
+output_message = ''
 
-image_extensions = [".png", ".jpg", ".jpeg", ".tif", ".tga", ".webp"]
+image_extensions = [".png", ".jpg", ".jpeg", ".tif", ".tga", ".webp", ".PNG", ".JPG"]
 video_extensions = [".mkv", ".mov", ".mp4", ".webm", ".MP4", ".MOV"]
 compressed_extensions = [".zip", ".7z", ".rar"]
-document_extensions = [".pdf", ".epub", ".txt", ".html", ".docx", ".csv", ".psd"]
-
+document_extensions = [".pdf", ".epub", ".txt", ".html", ".docx", ".csv", ".psd", ".json"]
 
 def organize_files(path, type='images', extension_list=image_extensions):
-    """Separa os arquivos de um diretório, criando novas pastas.
-    'path' = o caminho do diretório a ser organizado
-    'type' = nome da pasta que será criada e onde os arquivos serão movidos
-    'extension_list' = lista das extensões de arquivos a serem separados
-    """
-
+    global output_message
     os.makedirs(f'{path}/{type}', exist_ok=True)
-
-    output_message = Path("output_log.txt").read_text()
 
     for file_name in os.listdir(path):
 
@@ -39,29 +29,34 @@ def organize_files(path, type='images', extension_list=image_extensions):
             except shutil.Error:
                 print(f"{file_name} um arquivo de mesmo nome já existe na pasta")
 
-    Path("output_log.txt").write_text(output_message)
-
-
 def select_folder():
     folder = filedialog.askdirectory()
     return folder
 
-
 def start_organizing():
+    global output_message
+    output_message = ''
     folder = select_folder()
     organize_files(folder, 'images', image_extensions)
     organize_files(folder, 'videos', video_extensions)
     organize_files(folder, 'compressed', compressed_extensions)
     organize_files(folder, 'documents', document_extensions)
-
+    Path("output_log.txt").write_text(output_message)
 
 root = tk.Tk()
+root.geometry('800x600')
+
 frame = tk.Frame(root)
-frame.pack()
+frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 button = tk.Button(frame,
                    text="Organize",
                    command=start_organizing)
 button.pack(side=tk.LEFT)
+
+# Checkbox para ativar/desativar a remoção de duplicatas
+remove_duplicates_var = tk.IntVar()
+remove_duplicates_checkbox = tk.Checkbutton(frame, text="Remover duplicatas?", variable=remove_duplicates_var)
+remove_duplicates_checkbox.pack(side=tk.RIGHT)
 
 root.mainloop()
